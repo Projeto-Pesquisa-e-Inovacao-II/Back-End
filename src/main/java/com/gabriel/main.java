02da96d818ef9efa2365f96c42aa612c457c0833
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-04-30 17:51:31 -0300

    formating to send a file only

--- src/main/java/com/gabriel/main.java
+++ src/main/java/com/gabriel/main.java
@@ -12,80 +12,12 @@ public class main {
 
         DadosEvasaoService dadosEvasaoService = new DadosEvasaoService();
 
-        Integer year = 2024;
+        String filePath = "/home/ubuntu/Dados/L03_01-2024.xlsx";
 
-        //this is actually terrible, but it works
-        for (int conc = 1; conc < 33; conc++) {
+        dadosEvasaoService.carregarPlanilha(filePath);
 
-            year = 2024;
-            for (int i = 1; i <= 10; i++) {
-
-                Path doesFileExist;
-                String filePath = "";
-                if(conc < 10) {
-                    doesFileExist = Path.of("D:\\Downloads\\reactApp\\project2Sem\\dados\\dados_xlsx\\L0" + conc + "_" + year + ".xlsx"); // L0" + conc + "_" + year + ".xlsx"
-                } else {
-                    doesFileExist = Path.of("D:\\Downloads\\reactApp\\project2Sem\\dados\\dados_xlsx\\L" + conc + "_" + year + ".xlsx"); // L" + conc + "_" + year + ".xlsx"
-                }
-
-                if(Files.exists(doesFileExist)) {
-                    filePath = doesFileExist.toString();
-
-                    dadosEvasaoService.carregarPlanilha(filePath);
-                    System.out.println("35 -> existe");
-
-
-                    dadosEvasaoService.processarDados();
-                    dadosEvasaoService.inserirDadosEvasao(dadosEvasaoService.getDadosEvasaos(), conc, filePath);
-
-                } else {
-
-                    System.out.println("não");
-
-                    for (int j = 1; j <= 12; j++) {
-                        Path doesSubfilesExist;
-
-                        if(conc < 10) {
-                            if (j < 10) {
-                                doesSubfilesExist = Path.of("D:\\Downloads\\reactApp\\project2Sem\\dados\\dados_xlsx\\L0" + conc +"_0" + j + "-" + year + ".xlsx"); //L0" + conc +"_0" + j + "-" + year + ".xlsx"
-                            } else {
-                                doesSubfilesExist = Path.of("D:\\Downloads\\reactApp\\project2Sem\\dados\\dados_xlsx\\L0" + conc +"_" + j + "-" + year + ".xlsx");//L0" + conc +"_" + j + "-" + year + ".xlsx"
-                            }
-
-
-                        } else {
-                            doesSubfilesExist = Path.of("D:\\Downloads\\reactApp\\project2Sem\\dados\\dados_xlsx\\L" + conc +"_0" + j + "-" + year + ".xlsx");
-                        }
-
-
-                        if(Files.exists(doesSubfilesExist)) {
-                            System.out.println("62 -> existe");
-
-                            filePath = doesSubfilesExist.toString();
-
-                            dadosEvasaoService.carregarPlanilha(filePath);
-
-                            dadosEvasaoService.processarDados();
-                            dadosEvasaoService.inserirDadosEvasao(dadosEvasaoService.getDadosEvasaos(), conc, filePath);
-                        } else {
-
-                            System.out.println("nao");
-
-                        }
-
-                    }
-
-
-                }
-
-                year ++;
-                System.out.println(i);
-
-            }
-
-
-        }
-
+        dadosEvasaoService.processarDados();
+        dadosEvasaoService.inserirDadosEvasao(dadosEvasaoService.getDadosEvasaos(), 3, filePath);
 
     }
 }
