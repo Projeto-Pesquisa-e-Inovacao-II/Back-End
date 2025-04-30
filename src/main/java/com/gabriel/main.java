41d91fb04a89a4d3b88f68896ae8780b01cf823e
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-04-30 20:55:49 -0300

    initial code on log

--- src/main/java/com/gabriel/main.java
+++ src/main/java/com/gabriel/main.java
@@ -1,23 +1,37 @@
 package com.gabriel;
 
 import com.gabriel.services.DadosEvasaoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class main {
+    private static final Logger logger = LoggerFactory.getLogger(main.class);
+
     public static void main(String[] args) throws IOException {
         String NOME_BUCKET = System.getenv("NOME_BUCKET");
 
+        logger.info("Iniciando aplicação...");
+        logger.debug("Nome do bucket: {}", NOME_BUCKET);
+
         DadosEvasaoService dadosEvasaoService = new DadosEvasaoService();
 
-        String filePath = "/home/ubuntu/Dados/L03_01-2024.xlsx";
+        String filePath = "/home/gabriel/Documentos/2sem2/dados/dados_juntos/L03_01-2024.xlsx";
 
+        logger.info("Carregando planilha: {}", filePath);
         dadosEvasaoService.carregarPlanilha(filePath);
 
+        logger.info("Processando dados...");
         dadosEvasaoService.processarDados();
+
+        logger.info("Inserindo dados de evasão...");
         dadosEvasaoService.inserirDadosEvasao(dadosEvasaoService.getDadosEvasaos(), 3, filePath);
+
+        logger.info("Processo concluído com sucesso.");
+
 
     }
 }
