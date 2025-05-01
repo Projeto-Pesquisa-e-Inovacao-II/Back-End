16c56bfd349fe97711a109d654cd980602cc13cb
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-05-01 13:24:26 -0300

    initial code send file to s3

--- src/main/java/com/gabriel/main.java
+++ src/main/java/com/gabriel/main.java
@@ -1,25 +1,51 @@
 package com.gabriel;
 
+import com.gabriel.infra.S3Provider;
 import com.gabriel.services.DadosEvasaoService;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 public class main {
     private static final Logger logger = LoggerFactory.getLogger(main.class);
 
     public static void main(String[] args) throws IOException {
-        String NOME_BUCKET = System.getenv("NOME_BUCKET");
+        S3Client s3Client = new S3Provider().getS3Client();
+        String bucketName = "dados-dataway-dev";
+        String filePath = "D:\\Downloads\\reactApp\\project2Sem\\dados\\dados_xlsx\\L27_02-2024.xlsx";
+
+        try {
+            logger.info("Iniciando upload de arquivo para S3...");
+            logger.debug("Nome do bucket: {}", bucketName);
+            String uniqueFileName = UUID.randomUUID().toString();
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(uniqueFileName)
+                    .build();
+
+            File file = new File(filePath);
+            s3Client.putObject(putObjectRequest, RequestBody.fromFile(file));
+
+            logger.info("Arquivo '" + file.getName() + "' enviado com sucesso com o nome: " + uniqueFileName);
+        } catch (S3Exception e) {
+            logger.error("Erro ao fazer upload do arquivo: " + e.getMessage());
+        }
+
 
         logger.info("Iniciando aplicação...");
-        logger.debug("Nome do bucket: {}", NOME_BUCKET);
 
         DadosEvasaoService dadosEvasaoService = new DadosEvasaoService();
 
-        String filePath = "D:\\Downloads\\reactApp\\project2Sem\\dados\\dados_xlsx\\L03_01-2024.xlsx";
 
         logger.info("Carregando planilha: {}", filePath);
         dadosEvasaoService.carregarPlanilha(filePath);
@@ -29,6 +55,7 @@ public class main {
 
         logger.info("Inserindo dados de evasão...");
         dadosEvasaoService.inserirDadosEvasao(dadosEvasaoService.getDadosEvasaos(), 3, filePath);
+
 
         logger.info("Processo concluído com sucesso.");
 
