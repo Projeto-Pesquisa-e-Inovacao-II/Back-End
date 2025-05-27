ff985e18b5f6c3e143642e4b8ac943f25e29b53b
lrobatino <leandro.robatino@sptech.school>
2025-05-27 09:04:45 -0300

    correção .env conexaobanco

--- src/main/java/com/gabriel/infra/ConexaoBanco.java
+++ src/main/java/com/gabriel/infra/ConexaoBanco.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBanco {
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
     private static final String URL = dotenv.get("BD_URL");
     private static final String USER = dotenv.get("BD_USER");
     private static final String PASSWORD = dotenv.get("BD_PASSWORD");
